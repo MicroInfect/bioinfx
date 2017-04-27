3aa3c8a10b8b2760b8814b3f2631d27f7b1cfe58
jrjhealey <jrj.healey@gmail.com>
2017-04-27 12:38:39 +0100

    added script to extract named feature from genbank

---
+++ GBKExtractFeature.py
@@ -0,0 +1,16 @@
+from Bio import SeqIO
+import sys
+
+product = 'NADH dehydrogenase subunit 5'
+
+with open(sys.argv[2], 'w') as nfh:
+        for rec in SeqIO.parse(sys.argv[1], "genbank"):
+                if rec.features:
+                        for feature in rec.features:
+                                if feature.type == "CDS":
+					if feature.qualifiers['product'][0] == product:
+	                                        nfh.write(">%s|%s from %s\n%s\n" % (
+						feature.qualifiers['gene'][0],
+        	                                feature.qualifiers['product'][0],
+                	                        rec.name,
+						feature.location.extract(rec).seq))
